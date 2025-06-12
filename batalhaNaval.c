c70acb4d52af757202ec80a0a3c618f097f462ee
JudahLeite <judahprogramacao@gmail.com>
2025-06-12 21:59:25 +0000

    NÍVEL AVENTUREIRO - OK

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -23,33 +23,42 @@ int main() {
             tabuleiro[i][j]=3;
         }
     }
+    // Terceiro Navio.
+    for(int i =6; i<9;i++){
+        for(int j =6; j<9;j++ ){
+            if (i==j) {
+                tabuleiro[i][j]=3;
+            }            
+        }
+    }
 
+    // Quarto Navio.
+    for (int i=6;i<9;i++){
+        for (int j = 3; j>0;j--){
+            if (j+i==9){
+                tabuleiro[i][j]=3;
+            }
+        }
+    }
     //Imprimindo o Tabuleiro
 
-    printf("  "); //Cabeçalho
+    printf("   "); //Cabeçalho
     for(int i = 0; i<10; i++){
         printf("%c ",'A'+i); //Letras para colunas
     }
     printf("\n");
     
-    for(int i = 0; i<10; i++){
-        printf("%d ",i+1); // Números para linhas
+    for(int i = 1; i<10; i++){
+        printf("%2d ",1+i); // Números para linhas
         for (int j=0; j<10; j++ ){
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
     printf("\n");
-    printf("Navio 1 está na posição");
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
-
-    // Nível Mestre - Habilidades Especiais com Matrizes
+    
+    
+      // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
     // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
     // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
